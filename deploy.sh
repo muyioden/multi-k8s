d37aecbb6ae79a6b7276a462eca0ceb30f6742f8
muyioden <modenike@gmail.com>
2019-03-23 22:58:44 -0500

    Deployments revised

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,14 @@ docker build -t muyidocker/multi-client:latest -t muyidocker/multi-client:$SHA -
 docker build -t muyidocker/multi-server:latest -t muyidocker/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t muyidocker/multi-worker:latest -t muyidocker/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
 
-docker push muyidocker/multi-client
-docker push muyidocker/multi-server
-docker push muyidocker/multi-worker
+docker push muyidocker/multi-server:latest
+docker push muyidocker/multi-client:latest
+docker push muyidocker/multi-worker:latest
+docker push muyidocker/multi-server:$SHA
+docker push muyidocker/multi-client:$SHA
+docker push muyidocker/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=muyidocker/server
+kubectl set image deployments/server-deployment server=muyidocker/server:$SHA
+kubectl set image deployments/client-deployment client=muyidocker/client:$SHA
+kubectl set image deployments/worker-deployment worker=muyidocker/worker:$SHA
